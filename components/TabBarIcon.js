bdab72d1ad65c707365fc0dcd418b84992ac178a
Declan Nishiyama <dnishiyama@gmail.com>
2019-09-04 22:23:57 -0400

    Initial commit

---
+++ components/TabBarIcon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import Colors from '../constants/Colors';
+
+export default function TabBarIcon(props) {
+  if (props.name == 'food') {
+    return (
+      <MaterialCommunityIcons
+        name={props.name}
+        size={26}
+        style={{ marginBottom: -3 }}
+        color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+    );
+  } else {
+    return (
+      <Ionicons
+        name={props.name}
+        size={26}
+        style={{ marginBottom: -3 }}
+        color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+    );
+  }
+}
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  config
+);
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Info',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+HomeStack.path = '';
+
+const LinksStack = createStackNavigator(
+  {
+    Links: LinksScreen,
+  },
+  config
+);
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Foods',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name='food' />
+  ),
+};
+
+LinksStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
+  LinksStack,
+  HomeStack,
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
